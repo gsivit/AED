8c4915ac253a0b13fba3de0d9f2987742c1712df
gsivit <gsivit@gmail.com>
2020-04-20 22:56:20 -0300

    TP 00

---
+++ 00-CppHelloWorld/README.md
@@ -0,0 +1,9 @@
+﻿# Datos Front Page
+
+## Algoritmos y Estructuras de Datos
+
+- Curso: K1051
+- Año: 2020
+- Legajo: 160366-8
+- Apellido: Sivit
+- Nombre: Germán
---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,11 @@
+/* Hello.cpp
+ * Codigo fuente para el TP 00 "Hello, World!" en C++
+ * Germán Sivit
+ * 19/04/2020
+ */
+
+#include <iostream>
+int main()
+{
+    std::cout <<"Hello, world\n";
+} 
